b267231fd6d60f3c1af0c3f2ea5a9e40b22ec2ba
namjiyun <namjiyun3275@gmail.com>
2022-04-07 09:32:45 +0900

    [style] prettier 적용

--- 1st_seminar/level1/object.js
+++ 1st_seminar/level1/object.js
@@ -1,34 +1,33 @@
-const { cannotHaveAUsernamePasswordPort } = require("whatwg-url");
+const { cannotHaveAUsernamePasswordPort } = require('whatwg-url');
 
 const sopt = {
-    season: 30,
-    group: ['YB', 'OB'],
-    part: ['서버', '기획', '디자인', '안드로이드', '웹', 'iOS'],
-    president: '김규민',
-    introduce: function () {
-        this.part.map(partName => {
-            console.log(`솝트 내 파트는 ${partName} 파트가 있어요!`)
-        });
-    }
-}
+  season: 30,
+  group: ['YB', 'OB'],
+  part: ['서버', '기획', '디자인', '안드로이드', '웹', 'iOS'],
+  president: '김규민',
+  introduce: function () {
+    this.part.map(partName => {
+      console.log(`솝트 내 파트는 ${partName} 파트가 있어요!`);
+    });
+  },
+};
 console.log(sopt.group);
 sopt.introduce();
 
 console.log(sopt.season);
 
-let array = [1, true, "string"];
+let array = [1, true, 'string'];
 console.log(array);
 
 let array2 = [
-    {
-        name: '남지윤',
-        age: 5
-    },
-    {
-        name: '김소령',
-        age: 6
-    }
-]
+  {
+    name: '남지윤',
+    age: 5,
+  },
+  {
+    name: '김소령',
+    age: 6,
+  },
+];
 console.log(array2);
 console.log(typeof array2);
-
--- 1st_seminar/level1/variable.js
+++ 1st_seminar/level1/variable.js
@@ -1,24 +1,23 @@
-var varName = "남지윤";
-var varName = "장서현";
-varName = "채정아";
+var varName = '남지윤';
+var varName = '장서현';
+varName = '채정아';
 
-console.log("var : ",varName);
+console.log('var : ', varName);
 
-let letName = "남지윤";
+let letName = '남지윤';
 //let letName = "장서현";
-letName = "채정아"
+letName = '채정아';
 
-console.log("let: ", letName);
+console.log('let: ', letName);
 
-const constName = "남지윤";
+const constName = '남지윤';
 //const constName = "장서현";
-constName = "채정아";
+constName = '채정아';
 
-console.log("const: ", constName);
-
+console.log('const: ', constName);
 
 if (true) {
-    var x = 'var variable';
+  var x = 'var variable';
 }
 console.log(x);
 
@@ -31,12 +30,11 @@ console.log(y);
 console.log(z);
 */
 
-function foo () {
-    if (true) {
-        var name = "채정아"
-        console.log('if - block - ', name);
-    }
-    console.log('function - block - ', name);
+function foo() {
+  if (true) {
+    var name = '채정아';
+    console.log('if - block - ', name);
+  }
+  console.log('function - block - ', name);
 }
 //console.log('global - ', name);
-
