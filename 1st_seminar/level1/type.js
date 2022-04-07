963c268452d3044cfa3bf22362fc2883b97f7f23
namjiyun <namjiyun3275@gmail.com>
2022-04-07 09:32:45 +0900

    [style] [prettier 적용

--- 1st_seminar/level1/type.js
+++ 1st_seminar/level1/type.js
@@ -9,12 +9,10 @@ let server = true;
 console.log('안녕하세요 제 이름은 ' + name + '입니다. 제 나이는 ' + age + '살 입니다.');
 console.log(`안녕하세요 제 이름은 ${name}입니다. 제 나이는 ${age}살 입니다.`);
 
-
 console.log(typeof null);
 console.log(typeof undefined);
 
-let arr = ["안녕", 1, "나는", 2, true];
-
+let arr = ['안녕', 1, '나는', 2, true];
 
 //map : 새로운 배열 만들 때 유용
 let num = [1, 2, 3, 4];
@@ -23,10 +21,9 @@ const numArr = num.map(x => x * 2);
 console.log(numArr);
 
 numArr.map(x => {
-    console.log(x);
-})
-
+  console.log(x);
+});
 
 for (const x of numArr) {
-    console.log(x);
+  console.log(x);
 }
--- 1st_seminar/level1/type.ts
+++ 1st_seminar/level1/type.ts
@@ -3,43 +3,38 @@ let grade: number = 4;
 let isDeleted: boolean = false;
 
 const sum = (x: number, y: number) => {
-    return x * y;
-}
+  return x * y;
+};
 
 const numArray: number[] = [1, 2, 3, 4, 5];
 const numArray2: Array<number> = [1, 2, 3, 4];
 
-const strArray: string[] = ["hi", "hello"];
-const strArray2: Array<string> = ["hi2", "hello2"];
+const strArray: string[] = ['hi', 'hello'];
+const strArray2: Array<string> = ['hi2', 'hello2'];
 
 //원시 타입은 할당 불가능
-const obj: object = {
-
-}
+const obj: object = {};
 //모든 타입 할당 가능
-const obj2: Object = {
-  
-}
+const obj2: Object = {};
 
 const f1 = (obj: object): void => {
-    console.log(obj);
-}
+  console.log(obj);
+};
 const f2 = (obj: Object): void => {
-    console.log(obj);
-}
+  console.log(obj);
+};
 f2([1, 2, 3, 4]);
 f2('hihi');
 
 f1([1, 2, 3, 4]);
 //f1('hihi');
 
-
 const div = (x: number, y: number): number => {
-    return x / y;
-}
+  return x / y;
+};
 const div2 = (x: number, y: number): number[] => {
-    return [x / y];
-}
+  return [x / y];
+};
 
 let p: null = null;
 let u: undefined = undefined;
@@ -53,8 +48,8 @@ let asName: any = '서버';
 let asLength: number = (asName as string).length;
 
 const unknown: any = {
-    name: '채정아',
-    age: 18,
-    organization: 'SOPT',
-    completion: [28, 29]
-}
+  name: '채정아',
+  age: 18,
+  organization: 'SOPT',
+  completion: [28, 29],
+};
